f72c1b464ed4ad9ffb65c2be0c6371bdd6515f3a
BrooksNuss <bnussjr@gmail.com>
2019-08-09 02:11:57 -0400

    Full VAD setup in the UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { RTCService } from './services/rtc.service';
 
 @Component({
@@ -6,22 +6,33 @@ import { RTCService } from './services/rtc.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   @ViewChild('audioElement') audioElement: ElementRef<HTMLAudioElement>;
   inputStreamTrack: MediaStreamTrack;
   inputStream: MediaStream;
   remoteConnection: RTCPeerConnection;
   title = 'ServerBrowserUI';
   audioContext: AudioContext;
+  inputVolumeNode: GainNode;
+  vadNode: AudioWorkletNode;
+  outputVolumeNode: GainNode;
+  sendAudioNode: MediaStreamAudioDestinationNode;
+  receiveAudioNode: MediaStreamAudioDestinationNode;
+  trackReady = false;
 
   constructor(private rtcService: RTCService) {}
+
+  ngOnInit() {
+    // this.initializeAudio();
+  }
 
   initializeAudio() {
     this.audioContext = new AudioContext();
     navigator.mediaDevices.getUserMedia({audio: ({autoGainControl: false} as any), video: false}).then(stream => {
       this.inputStream = stream;
       this.inputStreamTrack = stream.getAudioTracks()[0];
-      this.connectAudio();
+      this.initAudioInput(stream);
+      // this.connectAudio();
     }, err => {
       console.log('Error getting audio input');
       console.log(err);
@@ -42,11 +53,12 @@ export class AppComponent {
       // set remote description
       this.remoteConnection.setRemoteDescription(offer.localDescription).then(() => {
         // add our media capabilities
-        this.remoteConnection.addTrack(this.inputStreamTrack, this.inputStream);
+        this.remoteConnection.addTrack(this.sendAudioNode.stream.getAudioTracks()[0], this.sendAudioNode.stream);
         // create answer
         this.remoteConnection.createAnswer().then((answer: RTCSessionDescription) => {
           this.remoteConnection.setLocalDescription(answer).then(() => {
             this.rtcService.sendAnswer(id, this.remoteConnection.localDescription).subscribe(res => {
+              this.trackReady = true;
               this.remoteConnection.onconnectionstatechange = e => {
                 if (this.remoteConnection.connectionState === 'connected') {
                   this.initAudioOutput();
@@ -62,19 +74,59 @@ export class AppComponent {
     });
   }
 
+  initAudioInput(stream: MediaStream) {
+    let inputNode = this.audioContext.createMediaStreamSource(stream);
+    this.inputVolumeNode = this.audioContext.createGain();
+    this.sendAudioNode = this.audioContext.createMediaStreamDestination();
+    this.audioContext.audioWorklet.addModule('../assets/Scripts/vad.js').then(() => {
+      this.vadNode = new AudioWorkletNode(this.audioContext, 'VadProcessor', {parameterData: {sensitivity: .03, falloff: 500}});
+      this.handleVadMessage();
+      inputNode.connect(this.vadNode);
+      this.vadNode.connect(this.inputVolumeNode);
+      // let sensitivityParam = (this.vadNode.parameters as any).get('sensitivity');
+      this.inputVolumeNode.connect(this.sendAudioNode);
+      this.audioElement.nativeElement.srcObject = this.sendAudioNode.stream;
+      this.audioElement.nativeElement.muted = true;
+      // this.sendAudioNode.connect(this.audioContext.destination);
+      // this.inputVolumeNode.connect(this.audioContext.destination);
+      this.connectAudio();
+    });
+  }
+
   initAudioOutput() {
       let remoteStream = new MediaStream();
       remoteStream.addTrack(this.remoteConnection.getReceivers().find(r => r.track.kind === 'audio').track);
-      this.audioElement.nativeElement.srcObject = remoteStream;
-      this.audioElement.nativeElement.muted = true;
+      // this.audioElement.nativeElement.srcObject = remoteStream;
+      // this.audioElement.nativeElement.muted = true;
+      // maybe do this beforehand so the user can modify it
+      this.outputVolumeNode = this.audioContext.createGain();
       let remoteSource = this.audioContext.createMediaStreamSource(remoteStream);
-      let volumeNode = this.audioContext.createGain();
-      volumeNode.gain.value = 10;
-      remoteSource.connect(volumeNode);
-      volumeNode.connect(this.audioContext.destination);
+      this.outputVolumeNode.gain.value = 1;
+      remoteSource.connect(this.outputVolumeNode);
+      this.outputVolumeNode.connect(this.audioContext.destination);
+  }
+
+  handleVadMessage() {
+    let track;
+    this.vadNode.port.onmessage = msg => {
+      if (this.trackReady && !track) {
+        track = this.remoteConnection.getSenders()[0].track;
+      }
+      if (msg.data === 'activate') {
+        this.remoteConnection.getSenders()[0].replaceTrack(track);
+        // this.sendAudioNode.stream.getAudioTracks()[0].enabled = true;
+      } else if (msg.data === 'deactivate') {
+        // this.sendAudioNode.stream.getAudioTracks()[0].enabled = false;
+        this.remoteConnection.getSenders()[0].replaceTrack(null)
+      }
+    }
   }
 
   debugPause() {
     console.log('pausing');
+  }
+
+  ngOnDestroy() {
+    console.log('DESTROY');
   }
 }
---
+++ src/assets/Scripts/vad.js
@@ -0,0 +1,62 @@
+class VadProcessor extends AudioWorkletProcessor {
+  sensitivityValue = 0;
+  falloffValue = 0;
+  currentFalloffTime = 0;
+  active = false;
+  constructor(options) {
+    super();
+    console.log(options);
+    this.sensitivityValue = options.parameterData.sensitivity;
+    this.falloffValue = options.parameterData.fallOff;
+  }
+
+  static get parameterDescriptors() {
+    return [
+      {
+        name: 'sensitivity',
+        defaultValue: this.sensitivityValue,
+        minValue: 0,
+        maxValue: 1,
+        automationRate: 'k-rate'
+      },
+      {
+        name: 'falloff',
+        defaultValue: this.falloffValue,
+        minValue: 0,
+        maxValue: 1000,
+        automationRate: 'k-rate'
+      }
+    ]
+  }
+
+  process(inputs, outputs, parameters) {
+    let max = 0;
+    inputs[0].forEach((inputChannel, index) => {
+      const outputChannel = outputs[0][index];
+      inputChannel.forEach((value, index) => {
+        outputChannel[index] = value;
+        let absVal = Math.abs(value);
+        if (absVal > max) {
+          max = absVal;
+        }
+      })
+    })
+    if (!this.active && max > parameters.sensitivity[0]) {
+      this.active = true;
+      this.port.postMessage('activate');
+      this.currentFalloffTime = parameters.falloff[0];
+    } else if (this.active) {
+      if (max > parameters.sensitivity[0]) {
+        this.currentFalloffTime = parameters.falloff[0];
+      }
+      this.currentFalloffTime-= 2.6;
+      if (this.currentFalloffTime <= 0) {
+        this.active = false;
+        this.port.postMessage('deactivate');
+      }
+    }
+    return true;
+  }
+}
+
+registerProcessor('VadProcessor', VadProcessor);
